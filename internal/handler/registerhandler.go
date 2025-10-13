2d031b58a178eafe941ffbb6639ae8503b13af53
zhaoxin26 <zhaoxin26@baidu.com>
2025-10-13 17:27:03 +0800

    ping and auth

---
+++ internal/handler/registerhandler.go
@@ -0,0 +1,31 @@
+// Code scaffolded by goctl. Safe to edit.
+// goctl 1.9.2
+
+package handler
+
+import (
+	"net/http"
+
+	"deutsch/internal/logic"
+	"deutsch/internal/svc"
+	"deutsch/internal/types"
+	"github.com/zeromicro/go-zero/rest/httpx"
+)
+
+func RegisterHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.RegisterRequest
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+			return
+		}
+
+		l := logic.NewRegisterLogic(r.Context(), svcCtx)
+		resp, err := l.Register(&req)
+		if err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+		} else {
+			httpx.OkJsonCtx(r.Context(), w, resp)
+		}
+	}
+}
---
+++ internal/logic/logoutlogic.go
@@ -0,0 +1,33 @@
+// Code scaffolded by goctl. Safe to edit.
+// goctl 1.9.2
+
+package logic
+
+import (
+	"context"
+
+	"deutsch/internal/svc"
+	"deutsch/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type LogoutLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewLogoutLogic(ctx context.Context, svcCtx *svc.ServiceContext) *LogoutLogic {
+	return &LogoutLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *LogoutLogic) Logout() (resp *types.LogoutResponse, err error) {
+	// todo: add your logic here and delete this line
+
+	return
+}
