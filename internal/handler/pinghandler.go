b8f75db771567e742d0e0b598c24143e7deb8b31
zhaoxin26 <zhaoxin26@baidu.com>
2025-10-14 17:08:31 +0800

    add utils and jwt todo

--- internal/handler/pinghandler.go
+++ internal/handler/pinghandler.go
@@ -8,20 +8,13 @@ import (
 
 	"deutsch/internal/logic"
 	"deutsch/internal/svc"
-	"deutsch/internal/types"
 	"github.com/zeromicro/go-zero/rest/httpx"
 )
 
 func pingHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var req types.String
-		if err := httpx.Parse(r, &req); err != nil {
-			httpx.ErrorCtx(r.Context(), w, err)
-			return
-		}
-
 		l := logic.NewPingLogic(r.Context(), svcCtx)
-		err := l.Ping(&req)
+		err := l.Ping()
 		if err != nil {
 			httpx.ErrorCtx(r.Context(), w, err)
 		} else {
